19a72b34fa8fda513609f1fa720a2d82e18f61ee
Ikrom Murodov <ikrommurodov2001@gmail.com>
2020-11-03 10:05:38 -0700

    Сonnected all components to the table page.

---
+++ src/ts/components/index.ts
@@ -0,0 +1,4 @@
+export { Header } from './header';
+export { Toolbar } from './toolbar';
+export { Formula } from './formula';
+export { Table } from './table';
--- src/ts/pages/TablePage/index.ts
+++ src/ts/pages/TablePage/index.ts
@@ -1,7 +1,19 @@
 import { IPage, IPageParams } from 'router-for-dom';
 import { $ } from 'helper-for-dom';
+import { IComponent } from '@/core/interface';
+
+// components for table page
+import * as Components from '@/components';
+
+const arrayOfComponents = Object.values(Components);
 
 export class TablePage implements IPage {
+  /**
+   * This field stores instances of classes.
+   * @private - This field is private
+   */
+  private classInstances: IComponent[] = [];
+
   constructor(private params: IPageParams) {
     this.init();
   }
@@ -12,6 +24,17 @@ export class TablePage implements IPage {
     const $root = $.create('div', 'excel');
     const $childRoot = $.create('div', 'excel__container');
 
+    this.classInstances = arrayOfComponents.map((Component) => {
+      const $wrapperEl = $.create('div', Component.className);
+
+      const component = new Component();
+
+      $wrapperEl.html(component.toHtml());
+      $childRoot.append($wrapperEl);
+      return component;
+    });
+
+    $root.append($childRoot);
     return $root.$el;
   }
 }
